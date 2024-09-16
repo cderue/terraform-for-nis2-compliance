a9aa099cfcfa0ca1f3a399470262e281262f91e9
CEDRIC DERUE <cedric.derue@altran.com>
2024-09-16 19:28:22 +0200

    protection

--- org.tf
+++ org.tf
@@ -144,15 +144,15 @@ EOF
     overwrite_on_create = true
 }
 
-#resource "github_branch_protection_v3" "example" {
-#    repository     = github_repository.example.name
-#    branch         = "main"
-    #require_signed_commits = true
-#    require_conversation_resolution = true
+resource "github_branch_protection_v3" "example" {
+    repository     = github_repository.example.name
+    branch         = "main"
+    require_signed_commits = true
+    require_conversation_resolution = true
 #    enforce_admins = true # Ensure repository does not allow bypassing of branch protections rules
     #required_pull_request_reviews {
     #    dismiss_stale_reviews = true
     #    require_code_owner_reviews = true
     #    required_approving_review_count = 1
     #}
-#}
+}
