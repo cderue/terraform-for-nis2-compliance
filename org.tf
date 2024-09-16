6110625cacf47aeedd7c7b2778137d62122d7187
CEDRIC DERUE <cedric.derue@altran.com>
2024-09-16 19:32:01 +0200

    Ensure status checks are passing before merging PRs on the default branch

--- org.tf
+++ org.tf
@@ -149,10 +149,10 @@ resource "github_branch_protection_v3" "example" {
     branch         = "main"
     require_signed_commits = true
     require_conversation_resolution = true
-    enforce_admins = true # Ensure repository does not allow bypassing of branch protections rules
-    #required_pull_request_reviews {
-    #    dismiss_stale_reviews = true
-    #    require_code_owner_reviews = true
-    #    required_approving_review_count = 1
-    #}
+    enforce_admins = true
+    required_status_checks {
+        strict = true
+        contexts = ["continuous-integration"]
+    }
 }
+
