a0ee4a433510e34b4870778256f06a5bb9660037
Cédric Derue <cedric.derue@altran.com>
2024-09-23 09:48:10 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "github_token" {
     description = "GitHub token for authentication"
     sensitive   = true
 }
+
+variable "app_id" {
+    type        = string
+    description = "GitHub token for authentication"
+    sensitive   = true
+}
+
+variable "app_installation_id" {
+    type        = string
+    description = "GitHub token for authentication"
+    sensitive   = true
+}
+
+variable "app_pem_file" {
+    type        = string
+    description = "GitHub token for authentication"
+    sensitive   = true
+}
